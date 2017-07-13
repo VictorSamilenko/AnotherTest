b6affc17744fc6689fa1b33737c9ef5f126f4d23
Victor Vladimirovich <Samilenkov@gmail.com>
2017-07-13 13:26:42 +0300

    refactoring

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,64 +2,38 @@ import org.apache.log4j.Logger;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Event {
     private static Logger log = Logger.getLogger(Event.class);
-    private final SimpleDateFormat dateFormat;
-    private final Date morning, day, evening, night;
+    private static int MORNING = 6, DAY = 9, EVENING = 19, NIGHT = 23;
     private final ResourceBundle resourceBundle;
+    private final Calendar calendar;
 
-
-    public Event() throws ParseException {
-        try {
-            dateFormat = new SimpleDateFormat("HH:mm:ss");
-            morning = dateFormat.parse("06:00:00");
-            day = dateFormat.parse("09:00:00");
-            evening = dateFormat.parse("19:00:00");
-            night = dateFormat.parse("23:00:00");
-            resourceBundle = ResourceBundle.getBundle("bundle", Locale.getDefault(), new UTF8Control());
-        } catch (ParseException e) {
-            log.error("Ошибка парсинга даты");
-            throw e;
-        }
+    public Event() {
+        resourceBundle = ResourceBundle.getBundle("bundle", Locale.getDefault(), new UTF8Control());
+        calendar = Calendar.getInstance();
     }
 
-    public String getPart(Date currentDate) throws ParseException {
-        Date currentTime = null;
-        try {
-            currentTime = dateFormat.parse(dateFormat.format(currentDate));
-        } catch (ParseException e) {
-            log.error("Ошибка парсинга даты");
-            e.printStackTrace();
-            throw new ParseException(e.getMessage(), e.getErrorOffset());
-        }
-
+    public String getPart(Date currentDate) {
         log.info("Определение времени суток");
+        calendar.setTime(currentDate);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
 
-        if (currentTime.equals(morning) || (currentTime.after(morning) && currentTime.before(day)))
+        if (hour >= MORNING && hour < DAY)
             return "morning";
-
-        if (currentTime.equals(day) || (currentTime.after(day) && currentTime.before(evening)))
+        if (hour >= DAY && hour < EVENING)
             return "day";
-
-        if (currentTime.equals(evening) || (currentTime.after(evening) && currentTime.before(night)))
+        if (hour >= EVENING && hour < NIGHT)
             return "evening";
-
         return "night";
     }
 
     public String getMessage(Date date) {
-        String part = null;
-        try {
-            part = getPart(date);
-        } catch (ParseException e) {
-            if (part == null)
-                log.error("Ошибка парсинга, невозможно определить время суток.");
-            return null;
-        }
+        String part = getPart(date);
         String message = resourceBundle.getString("message_" + part);
         log.info("Создано новое сообщение!");
         return message;
--- src/test/java/TestWorld.java
+++ src/test/java/TestWorld.java
@@ -7,92 +7,92 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TestWorld {
-    private static SimpleDateFormat dateFormat  = new SimpleDateFormat("HH:mm:ss");
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     private static Event event;
 
-    private void testEvent(String formatDate,String partDay) throws ParseException {
-        Date date = dateFormat.parse(formatDate);
+    private void testEvent(String formatDate, String partDay) {
+        Date date;
+        try {
+            date = dateFormat.parse(formatDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return;
+        }
         String part = event.getPart(date);
         Assert.assertEquals(part, partDay);
     }
 
     @BeforeClass
-    public static void beforeTest() throws ParseException {
+    public static void beforeTest() {
         event = new Event();
     }
 
 
     @Test
-    public void testCreateEvent() throws ParseException {
+    public void testCreateEvent() {
         event = new Event();
-        try {
-            event.getPart(new Date());
-        } catch (ParseException e) {
-            Assert.fail();
-        }
+        event.getPart(new Date());
     }
 
 
     @Test
-    public void testMorning1() throws ParseException {
-        testEvent("06:00:00","morning");
+    public void testMorning1() {
+        testEvent("06:00:00", "morning");
     }
 
     @Test
-    public void testMorning2() throws ParseException {
-        testEvent("08:59:59","morning");
+    public void testMorning2() {
+        testEvent("08:59:59", "morning");
     }
 
     @Test
-    public void testMorning() throws ParseException {
-        testEvent("07:35:35","morning");
+    public void testMorning() {
+        testEvent("07:35:35", "morning");
     }
 
     @Test
-    public void testDay1() throws ParseException {
-        testEvent("09:00:00","day");
+    public void testDay1() {
+        testEvent("09:00:00", "day");
     }
 
     @Test
-    public void testDay2() throws ParseException {
-        testEvent("12:34:56","day");
+    public void testDay2() {
+        testEvent("12:34:56", "day");
     }
 
     @Test
-    public void testDay3() throws ParseException {
-        testEvent("18:59:59","day");
+    public void testDay3() {
+        testEvent("18:59:59", "day");
     }
 
     @Test
-    public void testEvening1() throws ParseException {
-        testEvent("19:00:00","evening");
+    public void testEvening1() {
+        testEvent("19:00:00", "evening");
     }
 
     @Test
-    public void testEvening2() throws ParseException {
-        testEvent("20:21:22","evening");
+    public void testEvening2() {
+        testEvent("20:21:22", "evening");
     }
 
     @Test
-    public void testEvening3() throws ParseException {
-        testEvent("22:59:59","evening");
+    public void testEvening3() {
+        testEvent("22:59:59", "evening");
     }
 
     @Test
-    public void testNight1() throws ParseException {
-        testEvent("23:00:00","night");
+    public void testNight1() {
+        testEvent("23:00:00", "night");
     }
 
     @Test
-    public void testNight2() throws ParseException {
-        testEvent("04:00:01","night");
+    public void testNight2() {
+        testEvent("04:00:01", "night");
     }
 
     @Test
-    public void testNight3() throws ParseException {
-        testEvent("05:59:59","night");
+    public void testNight3() {
+        testEvent("05:59:59", "night");
     }
-
-
 }
